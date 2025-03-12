1cb646a5ef5695cf5a780c85bd99c3d2e9cc722d
Mir Saif Ali <mir.saif.ali2004@gmail.com>
2025-03-12 12:41:36 +0530

    implement job search functionality with Hero and JobListing components; context for search state management

--- src/context/Appcontext.jsx
+++ src/context/Appcontext.jsx
@@ -1,11 +1,19 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const Appcontext = createContext()
 
 export const AppContextProvider = (props) => {
+
+    const [searchFilter, setSearchFilter] = useState({
+        title:'',
+        location:''
+    })
+
+    const [isSearched, setIsSearched] = useState(false)
     
     const value = {
-
+        setSearchFilter,searchFilter,
+        isSearched, setIsSearched
     }
 
     return (
