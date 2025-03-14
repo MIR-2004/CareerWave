fe23c89da8af232f697980b98a6982d45f252e2a
Mir Saif Ali <mir.saif.ali2004@gmail.com>
2025-03-14 10:49:39 +0530

    add Loading component; integrate k-convert and moment for job details in ApplyJob page; enhance JobCard and Navbar with cursor styles; update package dependencies

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
 
     const { openSignIn } = useClerk()
     const { user } = useUser()
+
+    const navigate = useNavigate()
     return (
         <div className='shadow py-4'>
             <div className='container px-4 2xl:px-20 mx-auto flex justify-between items-center'>
-                <img src={assets.logo} alt="" />
+                <img onClick={() => navigate('/')} className='cursor-pointer' src={assets.logo} alt="" />
                 {
                     user ? (
                         <div className='flex items-center gap-3'>
@@ -23,7 +25,7 @@ const Navbar = () => {
                         <div>
                             <div className='flex gap-4 max-sm:text-xs'>
                                 <button className='text-gray-600'>Recruiter Login</button>
-                                <button onClick={(e) => { openSignIn() }} className='bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full'>LogIn</button>
+                                <button onClick={(e) => { openSignIn() }} className='bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full cursor-pointer'>LogIn</button>
                             </div>
                         </div>
                     )
