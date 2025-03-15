989a874571f9f388d204d89cf04c0aa7eedf0f5d
Mir Saif Ali <mir.saif.ali2004@gmail.com>
2025-03-15 10:40:57 +0530

    add job application history display; integrate moment for date formatting and enhance resume upload functionality

--- src/pages/Application.jsx
+++ src/pages/Application.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
-import { assets } from '../assets/assets'
+import { assets, jobsApplied } from '../assets/assets'
+import moment from 'moment'
 
 const Application = () => {
 
   const [isEdit, setIsEdit] = useState(false)
   const [resume, setResume] = useState(null)
+
 
 
   return (
@@ -21,7 +23,7 @@ const Application = () => {
             <input id='resumeUpload' onChange={e => setResume(e.target.files[0])} accept='application/pdf' type="file" hidden />
             <img src={assets.profile_upload_icon} alt="" />
           </label>
-          <button className='bg-gray-100 border border-green-400 rounded-lg px-4 py-2'>Save</button>
+          <button onClick={e => setIsEdit(false)} className='bg-gray-100 border border-green-400 rounded-lg px-4 py-2'>Save</button>
           </>) : (<div className='flex gap-2'>
             <a className='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg cursor-pointer' href="">
               Resume
@@ -32,6 +34,41 @@ const Application = () => {
           </div>)
         }
       </div>
+      <h2 className='text-xl font-semibold mb-4'>Jobs Applied</h2>
+      <table className='min-w-full bg-white border rounded-lg'>
+        <thead>
+          <tr>
+            <th className='py-3 px-4 border-b text-left '>Company</th>
+            <th className='py-3 px-4 border-b text-left '>Job Title</th>
+            <th className='py-3 px-4 border-b text-left max-sm:hidden '>Location</th>
+            <th className='py-3 px-4 border-b text-left max-sm:hidden '>Date</th>
+            <th className='py-3 px-4 border-b text-left '>Status
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {jobsApplied.map((job, index) => 
+            true ? (
+              <tr>
+                <td className='py-3 px-4 flex items-center gap-2 border-b'>
+                  <img className='w-8 h-8' src={job.logo} alt="" />
+                  {job.company}
+                </td>
+                <td className='py-2 px-4 border-b'>{job.title}</td>
+                <td className='py-2 px-4 border-b max-sm:hidden'>{job.location}</td>
+                <td className='py-2 px-4 border-b max-sm:hidden'>{moment(job.date).format('ll')}</td>
+                <td className='py-2 px-4 border-b'>
+                <span className={`${job.status === 'Accepted' ? 'bg-green-100' : job.status === 'Rejected' ? 'bg-red-100' : 'bg-yellow-100'} px-4 py-1.5 rounded`}>
+                {job.status}
+              </span>
+                </td>
+              </tr>
+            ) : (
+              null
+            )
+          )}
+        </tbody>
+      </table>
     </div>
     
     </>
