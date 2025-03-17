e6c1e427d8667400737a6d8bbf10b2a594f7b510
Mir Saif Ali <mir.saif.ali2004@gmail.com>
2025-03-17 11:21:07 +0530

    enhance RecruiterLogin component; implement form submission handling, image upload for company logo, and context integration

--- src/components/RecruiterLogin.jsx
+++ src/components/RecruiterLogin.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
+import {Appcontext} from '../context/Appcontext'
+
 
 const RecruiterLogin = () => {
 
@@ -11,12 +13,39 @@ const RecruiterLogin = () => {
 
   const [isTextDataSubmited, setIsTextDataSubmited] = useState(false)
 
+  const {setShowRecruiterLogin} = useContext(Appcontext)
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault()
+    if (state === 'Sign Up' && !isTextDataSubmited) {
+      setIsTextDataSubmited(true)
+    }
+  }
+
+  useEffect (() => {
+    document.body.style.overflow = 'hidden'
+
+    return () => {
+      document.body.style.overflow = 'unset'
+    }
+  }, [])
+
   return (
     <div className='absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
-      <form className='relative bg-white p-10 rounded-xl text-slate-500'>
+      <form onSubmit={onSubmitHandler} className='relative bg-white p-10 rounded-xl text-slate-500'>
         <h1 className='text-center text-2xl text-neutral-700 font-medium'>Recruiter {state}</h1>
         <p className='text-sm '>Welcome Back! Please sign in to continue</p>
-        <>
+        {state === "Sign Up" && isTextDataSubmited ? (<>
+          
+          <div className='flex items-center gap-4 my-10'>
+            <label htmlFor='image'>
+              <img className='w-16 rounded-full' src={image ? URL.createObjectURL(image) : assets.upload_area} alt="" />
+              <input onChange={e => setImage(e.target.files[0])} type="file" id='image' hidden />
+            </label>
+            <p>Upload Company <br /> Logo </p>
+          </div>
+        
+        </>) : (<>
 
           {state !== 'Login' && (
             <div className='border px-4 py-2 flex items-center gap-2 rounded-full mt-5'>
@@ -33,14 +62,19 @@ const RecruiterLogin = () => {
             <input className='outline-none text-sm' onChange={e => setPassword(e.target.value)} value={password} type="password" placeholder='Password' required />
           </div>
 
-          <p className='text-sm text-blue-600 my-4 cursor-pointer'>Forgot password</p>
-        </>
+        </>)}
 
-        <button className='bg-blue-600 w-full text-white py-2 rounded-full'>
-          {state === 'Login' ? 'login' : 'create account'}
+        {state === 'Login' && <p className='text-sm text-blue-600 mt-4 cursor-pointer'>Forgot password</p>}
+
+        <button type='submit' className='bg-blue-600 w-full text-white py-2 rounded-full mt-4'>
+          {state === 'Login' ? 'login' : isTextDataSubmited ? 'create account' : 'Next' }
         </button>
 
         {state === 'Login' ? (<p className='mt-5 text-center'>Don't have an account? <span className='text-blue-600 cursor-pointer' onClick={() => setState('Sign Up')}>Sign Up</span></p>) : (<p className='mt-5 text-center'>Already have an account? <span className='text-blue-600 cursor-pointer' onClick={() => setState('Login')}>Login</span></p>)}
+      
+
+      <img onClick={e => setShowRecruiterLogin(false)} src={assets.cross_icon} className='absolute top-5 right-5 cursor-pointer' alt="" />
+      
       </form>
     </div>
   )
