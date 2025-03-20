bcf7f47db6619dd5ee3a95f9a778ab394b8f34ee
Mir Saif Ali <mir.saif.ali2004@gmail.com>
2025-03-20 14:18:27 +0530

    implement ManageJobs page with job listing table and navigation; enhance ViewApplications page layout

--- src/pages/ViewApplications.jsx
+++ src/pages/ViewApplications.jsx
@@ -30,8 +30,7 @@ const ViewApplications = () => {
                   <td className='p-2 px-4 border-b'>
                     <a href="" target='_blank' className='bg-blue-50 text-blue-400 px-3 py-1 rounded inline-flex gap-2 items-center'>Resume
                     <img src={assets.resume_download_icon} alt="" />
-                      </a> 
-                    
+                    </a> 
                   </td>
                   <td className=' py-2 px-4 border-b relative'>
                     <div className='relative inline-block text-left group'>
