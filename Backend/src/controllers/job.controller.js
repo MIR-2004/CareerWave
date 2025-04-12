011998fb7eaeea7b04923f23414f52d053c3f8ba
Mir Saif Ali <mir.saif.ali2004@gmail.com>
2025-04-12 22:54:14 +0530

    refactor job controller and routes; improve error handling and code clarity

--- Backend/src/controllers/job.controller.js
+++ Backend/src/controllers/job.controller.js
@@ -1,12 +1,15 @@
 import { Job } from "../models/job.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
+
 // Get all jobs 
 
 const getJobs = asyncHandler(async (req, res) => {
     try {
         const jobs = await Job.find({visible: true}).populate({path: "companyId", select:'-password'})
-
+        if(!jobs){
+            throw new Error("No jobs found")
+        }
         res.json({
             success: true,
             jobs
@@ -23,29 +26,25 @@ const getJobs = asyncHandler(async (req, res) => {
 // Get single job by id
 const getJobById = asyncHandler(async (req, res) => {
     try {
-        const {id} = req.params;
+        const {id} = req.params
 
-        const singleJob = await Job.findById(id).populate({path: "companyId", select:'-password'})
-        if(!singleJob){
+        const job = await Job.findById(id).populate({path: "companyId", select:'-password'})
+        if(!job){
             throw new Error("Job not found")
         }
-
         res.json({
             success: true,
-            singleJob
+            job
         })
-
-        
     } catch (error) {
-        res.json({
+        res.status(500).json({
             success: false,
             message: error.message
         })
-        
     }
-});
-
+})
 export {
-    getJobs,
-    getJobById
+    
+    getJobById,
+    getJobs
 }
--- Backend/src/routes/job.routes.js
+++ Backend/src/routes/job.routes.js
@@ -3,9 +3,9 @@ import { getJobById, getJobs } from '../controllers/job.controller.js';
 
 const router = express.Router();
 
-//Routes for all jobs data
-router.get('/', getJobs)
 //Routes to get a single job data
 router.get('/:id', getJobById)
+//Routes for all jobs data
+router.get('/', getJobs)
 
 export default router;
